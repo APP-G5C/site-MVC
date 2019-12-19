127d0299da52842e705ef9b80d6ba3182986e858
redleader14 <46784287+redleader14@users.noreply.github.com>
2019-12-19 11:18:15 +0100

    Add files via upload

---
+++ controleurs/messagerie.php
@@ -0,0 +1,54 @@
+<?php
+	session_start();
+	
+	include('./modele/requetes.utilisateurs.php');
+	if (!isset($_GET['fonction']) || empty($_GET['fonction'])) {
+		$function = "messagerie";
+	} else {
+		$function = $_GET['fonction'];
+	}
+	
+	
+	switch ($function) {
+		case 'messagerie':
+			$vue = 'messagerie_administrateur';
+			$liste = recupererTousLesMessages($bdd, $_SESSION['id']);
+			if (empty($liste)) {
+				$alerte = "Aucun message pour l'instant";
+			}
+			break;
+		case 'envoyer':
+			$vue = 'envoyer_message';
+			if (isset($_POST['mail'])) {
+				$mail = $_POST['mail'];
+//				$query = 'SELECT id FROM utilisateur WHERE mail = ' . $_POST['mail'];
+				
+				$req = $bdd->prepare('SELECT id FROM utilisateur WHERE mail = :mail');
+				$req->execute(array(
+					'mail' => $mail
+				));
+				$donnees = $req->fetch();
+				
+				
+				$date = date('Y-m-d H:i:s');
+//				$date = $date->format('Y-m-d H:i:s');
+				
+//				echo DateTime();
+//				$id_recepteur = $bdd->query($query)->fetch();
+//				$datetime = CurrentDateTime();
+				$req = $bdd->prepare('INSERT INTO messagerie(message,  id_envoyeur,date, id_recepteur) VALUES(?,?, ?,?)');
+				$req->execute(array($_POST['message'], $_SESSION['id'],$date, $donnees[0]));
+			}
+			echo 'joseph';
+			break;
+		default:
+			// si aucune fonction ne correspond au paramètre function passé en GET
+			$vue = "erreur404";
+			$title = "error404";
+			$message = "Erreur 404 : la page recherchée n'existe pas.";
+		
+	}
+	
+	
+	include('vues/' . $vue . '.php');
+	
