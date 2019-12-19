8011b2ffb5e1d47dd3f878cf3fe3438484240d5c
Stan-Le-Floch <55876315+Stan-Le-Floch@users.noreply.github.com>
2019-12-19 11:54:35 +0100

    Update Menu_administrateur.php

--- vues/Menu_administrateur.php
+++ vues/Menu_administrateur.php
@@ -3,18 +3,18 @@
 <head>
 	<meta charset="UTF-8">
 	<title>SmartBeing</title>
-	<link rel="stylesheet" href="http://localhost/site-MVC-master/style/Menu_utilisateurCSS.css">
+	<link rel="stylesheet" href="style/Menu_utilisateurCSS.css">
 </head>
 <body>
 <body background="Background.jpg">
 
 <div id="conteneur">
-	<div class="element"><a href="index.php?cible=capteurs">Capteurs <br/> <img src='http://localhost/site%20APP%20MVC/style/faq.png'></a></div>
-	<div class="element"><a href="FAQ.php">Donnees <br/> <img src='http://localhost/site%20APP%20MVC/style/faq.png'></a></div>
-	<div class="element"><a href="index.php?cible=messagerie">Messagerie <br/> <img src='http://localhost/site%20APP%20MVC/style/bouton_play.png'></a></div>
-	<div class="element"><a href="index.php?cible=utilisateurs&fonction=utilisateurs"> Utilisateurs <br/> <img src='http://localhost/site%20APP%20MVC/style/bouton_score.png'></a></div>
-	<div class="element"><a href="index.php?cible=utilisateurs&fonction=profilAdmin"> Profil <br/> <img src='http://localhost/site%20APP%20MVC/style/profil.png'></a></div>
-	<div class="element"><a href="index.php?cible=FAQ&fonction=FAQAdmin">FAQ <br/> <img src='http://localhost/site%20APP%20MVC/style/faq.png'></a></div>
+	<div class="element"><a href="index.php?cible=capteurs">Capteurs <br/> <img src='style/faq.png'></a></div>
+	<div class="element"><a href="FAQ.php">Donnees <br/> <img src='style/faq.png'></a></div>
+	<div class="element"><a href="index.php?cible=messagerie">Messagerie <br/> <img src='style/bouton_play.png'></a></div>
+	<div class="element"><a href="index.php?cible=utilisateurs&fonction=utilisateurs"> Utilisateurs <br/> <img src='style/bouton_score.png'></a></div>
+	<div class="element"><a href="index.php?cible=utilisateurs&fonction=profilAdmin"> Profil <br/> <img src='style/profil.png'></a></div>
+	<div class="element"><a href="index.php?cible=FAQ&fonction=FAQAdmin">FAQ <br/> <img src='style/faq.png'></a></div>
 	
 </div>
 </div>
