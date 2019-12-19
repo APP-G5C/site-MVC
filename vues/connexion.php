f4259bd04bc82b114e5f6e012d42eadfeb177b94
Stan-Le-Floch <55876315+Stan-Le-Floch@users.noreply.github.com>
2019-12-19 11:55:16 +0100

    Update connexion.php

--- vues/connexion.php
+++ vues/connexion.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<link rel="stylesheet" href="http://localhost/site-MVC-master/style/ConnexionCSS.css" media="screen" type="text/css"/>
+	<link rel="stylesheet" href="style/ConnexionCSS.css" media="screen" type="text/css"/>
 </head>
 <body background="Background.jpg">
 
