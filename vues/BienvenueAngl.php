cc8989abc6cb93e47acf9471a82b6a9f1ad0e4d6
Corentinpvt <55877088+Corentinpvt@users.noreply.github.com>
2019-12-20 14:00:11 +0100

    Create BienvenueAngl.php

---
+++ vues/BienvenueAngl.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>SmartBeing</title>
+	<link rel="stylesheet" href="style/BienvenueCSS.css">
+</head>
+<body background= "style/Background.jpg">
+<div id="conteneur">
+	<div class ="bouton"> <a href="index.php?cible=utilisateurs&fonction=connexion">Connexion</a> </div>
+	<div class="bouton"> <a href="FAQbis.php">?</a></div>
+</div>
+<div id="flex_container">
+	<div>
+		<h1>SmartBeing</h1>
+	</div>
+	<div>
+		<h2>Follow your health wherever you are</h2>
+	</div>
+</div>
+<style>
+	/* width */
+	::-webkit-scrollbar {
+		width: 10px;
+	}
+	
+	/* Track */
+	::-webkit-scrollbar-track {
+		background: #f1f1f1;
+		opacity: 0.5;
+	}
+	
+	/* Handle */
+	::-webkit-scrollbar-thumb {
+		background: #888;
+		opacity : .5;
+	}
+	
+	/* Handle on hover */
+	::-webkit-scrollbar-thumb:hover {
+		background: #555;
+		opacity: 0.5;
+		
+	}
+</style>
+</body>
+</html>
