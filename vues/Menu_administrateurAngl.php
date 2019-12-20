781b2070cb2fe3754b382a3ba08194cb1736c753
Corentinpvt <55877088+Corentinpvt@users.noreply.github.com>
2019-12-20 14:12:54 +0100

    Create Menu_administrateurAngl.php

---
+++ vues/Menu_administrateurAngl.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>SmartBeing</title>
+	<link rel="stylesheet" href="style/Menu_utilisateurCSS.css">
+</head>
+<body>
+<body background="Background.jpg">
+<div id="conteneur">
+	<div class="element"><a href="index.php?cible=capteurs">Sensors <br/> <img src='style/faq.png'></a></div>
+	<div class="element"><a href="FAQ.php">Datas <br/> <img src='style/faq.png'></a></div>
+	<div class="element"><a href="index.php?cible=messagerie">Mailbox <br/> <img src='style/bouton_play.png'></a></div>
+	<div class="element"><a href="index.php?cible=utilisateurs&fonction=utilisateurs"> Users <br/> <img src='style/bouton_score.png'></a></div>
+	<div class="element"><a href="index.php?cible=utilisateurs&fonction=profilAdmin"> Profile <br/> <img src='style/profil.png'></a></div>
+	<div class="element"><a href="index.php?cible=FAQ&fonction=FAQAdmin">Q&A <br/> <img src='style/faq.png'></a></div>
+	
+</div>
+</div>
+</div>
+</body>
+</body>
+</html>
