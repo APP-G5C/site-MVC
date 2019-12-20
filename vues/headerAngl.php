a0bf0630365bf7bd8bb864d2ff84676d8883d135
Corentinpvt <55877088+Corentinpvt@users.noreply.github.com>
2019-12-20 14:25:43 +0100

    Create headerAngl.php

---
+++ vues/headerAngl.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Title</title>
+	<link rel="stylesheet" href="http://localhost/site-MVC-master/style/headerCSS.css">
+</head>
+<body>
+<ul>
+	<li><a href="Tests.php">Tests</a></li>
+	<li><a href="Scores.php">Scores</a></li>
+	<li><a href="index.php?cible=utilisateurs&fonction=profil">Profile</a></li>
+	<li><a href="index.php?cible=FAQ&fonction=FAQ">Q&A</a></li>
+	<li><a href="index.php?cible=messagerie">Mailbox</a></li>
+</ul>
+</body>
+</html>
