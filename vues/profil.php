884202cbf29e0261470289f8cf441b3527fa8149
redleader14 <46784287+redleader14@users.noreply.github.com>
2019-12-20 16:18:59 +0100

    Update profil.php

--- vues/profil.php
+++ vues/profil.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 <?php include("header.php"); ?>
-<?php $repertoire = "http://localhost/site%20APP%20MVC/"; ?>
+
 
 
 
@@ -21,7 +21,7 @@
 		<h2>PROFIL</h2>
 		<div class="line"></div>
 		<br>
-		<p><img class="photoprofil" src="<?php echo $repertoire. $_SESSION['photo']; ?>" alt="photo de profil"></p>
+		<p><img class="photoprofil" src="<?php echo $_SESSION['photo']; ?>" alt="photo de profil"></p>
 		<div id="conteneurprofil">
 			
 			
