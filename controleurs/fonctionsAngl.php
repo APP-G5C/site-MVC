81969c1a5d2a451a06125d844b6322d9fe32609e
NathanG5C <55877065+NathanG5C@users.noreply.github.com>
2019-12-20 14:37:06 +0100

    Update fonctionsAngl.php

--- controleurs/fonctionsAngl.php
+++ controleurs/fonctionsAngl.php
@@ -1,1 +1,18 @@
-
+<?php
+function estUnEntier($int){
+	return is_numeric($int);
+}
+function estUneChaine($chaine){
+	if (empty($chaine)){
+		return false;
+	} else {
+		return is_string($chaine);
+	}
+}
+function estUnMotDePasse($chaine){
+	if (empty($chaine) || strlen($chaine) < 10){
+		return false;
+	} else {
+		return is_string($chaine);
+	}
+}
