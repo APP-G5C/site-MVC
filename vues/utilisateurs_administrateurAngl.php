45b0cd26e62a3ec71b4310de2dcce936521a474a
Corentinpvt <55877088+Corentinpvt@users.noreply.github.com>
2019-12-20 15:03:47 +0100

    Create utilisateurs_administrateurAngl.php

---
+++ vues/utilisateurs_administrateurAngl.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>SmartBeing</title>
+	<link rel="stylesheet" href="style/utilisateursCSS.css">
+</head>
+<body>
+<?php include("headerAdmin.php"); ?>
+<div class="bg">
+	<form action="#" method="POST">
+		<h2>Utilisateur</h2>
+		<div class="line"></div>
+		
+		<button class="button"><a style="color:white; text-decoration:none" href="enregistrement.php">Search an user</a></button>
+		<button class="button"><a style="color:white; text-decoration:none" href="index.php?fonction=CreerCompte">Create an account</a></button>
+	</form>
+</div>
+</body>
+</html>
