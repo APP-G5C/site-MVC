2ef197d072c3163941d7929ed8ece72250d27411
Corentinpvt <55877088+Corentinpvt@users.noreply.github.com>
2019-12-20 14:23:40 +0100

    Create connexionAngl.php

---
+++ vues/connexionAngl.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="style/ConnexionCSS.css" media="screen" type="text/css"/>
+</head>
+<body background="Background.jpg">
+	
+		<div id="container">
+			
+			<form action="" method="POST">
+				<h1>Connection</h1>
+				
+				<label><b>Email adress</b></label>
+				<input type="text" placeholder="Enter your email adress" name="mail" id="mail" required>
+				
+				<label><b>Password</b></label>
+				<input type="password" placeholder="Enter your password" name="password" id="password" required>
+				
+				<a href="index.php"><button type="submit"  name = "submit">Connection</button></a>
+			
+			</form>
+		</div>
+</body>
+</html>
