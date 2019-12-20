8fc6eec2655331571208353ca45cbdf56dcf1e06
NathanG5C <55877065+NathanG5C@users.noreply.github.com>
2019-12-20 14:45:37 +0100

    Update connexionAngl.php

--- modele/connexionAngl.php
+++ modele/connexionAngl.php
@@ -1,1 +1,9 @@
-
+<?php
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=site app;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}
+catch (Exception $e)
+{
+	die('Error : ' . $e->getMessage());
+}
