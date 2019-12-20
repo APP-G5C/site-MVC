1d0e72e805a1515d7772d838d1d9997f943eae07
Corentinpvt <55877088+Corentinpvt@users.noreply.github.com>
2019-12-20 14:04:13 +0100

    Create FAQ_administrateurAngl.php

---
+++ vues/FAQ_administrateurAngl.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>SmartBeing</title>
+	<link rel="stylesheet" href="style/FAQ_administrateurCSS.css">
+</head>
+<body>
+<?php include("headerAdmin.php"); ?>
+<div class="bg">
+	
+	<form action="" method="POST">
+		<h2>FAQ</h2>
+		<div class="line"></div>
+		
+		<h3>USUAL QUESTIONS</h3>
+		
+		
+		
+			
+			<?php foreach ($liste as $element) { ?>
+			
+				
+				<h4> <?php  echo $element['ID'] . '.     ' ; echo $element['Question'] . '<br />'; ?> </h4>
+				<p>
+					
+					<?php echo $element['Réponse'] . '<br />'; ?>
+				
+				</p>
+				
+				
+				<?php
+			} ?>
+			
+			$question->closeCursor(); // Termine le traitement de la requête
+		
+		<button class="button"><a style="color:white; text-decoration:none" href="index.php?cible=FAQ&fonction=modifierFAQ">Effectuer un changement</a>
+	
+	</form>
+</div>
+
+<style>
+	/* width */
+	::-webkit-scrollbar {
+		width: 10px;
+	}
+	
+	/* Track */
+	::-webkit-scrollbar-track {
+		background: #f1f1f1;
+		opacity: 0.5;
+	}
+	
+	/* Handle */
+	::-webkit-scrollbar-thumb {
+		background: #888;
+		opacity : .5;
+	}
+	
+	/* Handle on hover */
+	::-webkit-scrollbar-thumb:hover {
+		background: #555;
+		opacity: 0.5;
+		
+	}
+</style>
+</body>
+</html>
