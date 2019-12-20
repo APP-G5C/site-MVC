c72eaffbd1b4079d1e16d7d85a9ffe06cefe886f
Corentinpvt <55877088+Corentinpvt@users.noreply.github.com>
2019-12-20 14:14:17 +0100

    Create Menu_utilisateurAngl.php

---
+++ vues/Menu_utilisateurAngl.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>SmartBeing</title>
+	<link rel="stylesheet" href="http://localhost/site-MVC-master/style/Menu_utilisateurCSS.css">
+</head>
+<body>
+<body background="Background.jpg">
+<div id="conteneur">
+	<div class="element"><a href="Tests.php">Test <br/> <img src='http://localhost/site%20APP%20MVC/style/bouton_play.png'></a></div>
+	<div class="element"><a href="Scores.php"> Scores <br/> <img src='http://localhost/site%20APP%20MVC/style/bouton_score.png'></a></div>
+	<div class="element"><a href="index.php?cible=utilisateurs&fonction=profil"> Profile <br/> <img src='http://localhost/site%20APP%20MVC/style/profil.png'></a></div>
+	<div class="element"><a href="index.php?cible=FAQ&fonction=FAQ">Q&A <br/> <img src='http://localhost/site%20APP%20MVC/style/faq.png'></a></div>
+	<div class="element"><a href="index.php?cible=messagerie"> Mailbox <br/> <img src='http://localhost/site%20APP%20MVC/style/faq.png'></a></div>
+</div>
+</body>
+</body>
+</html>
