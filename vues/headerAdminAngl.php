60444f2b4e54b4d5e769275a097651661c1deedf
Corentinpvt <55877088+Corentinpvt@users.noreply.github.com>
2019-12-20 14:58:13 +0100

    Create headerAdminAngl.php

---
+++ vues/headerAdminAngl.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Title</title>
+	<link rel="stylesheet" href="style/headerCSS.css">
+</head>
+<body>
+<ul>
+	<li><a href="index.php?cible=capteurs"Sensors</a></li>
+	<li><a href="donnees_admin.php">Datas</a></li>
+	<li><a href="index.php?cible=messagerie">Mailbox</a></li>
+	<li><a href="index.php?cible=utilisateurs&fonction=utilisateurs">Users</a></li>
+	<li><a href="index.php?cible=utilisateurs&fonction=profilAdmin">Profile</a></li>
+	<li><a href="index.php?cible=FAQ&fonction=FAQAdmin">Q&A</a></li>
+	
+</ul>
+</body>
+</html>
