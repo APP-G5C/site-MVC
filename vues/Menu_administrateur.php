990035cab1d110cdfdfae700bd20a10183918d98
Stan-Le-Floch <55876315+Stan-Le-Floch@users.noreply.github.com>
2020-01-22 23:10:27 +0000

    Update Menu_administrateur.php

--- vues/Menu_administrateur.php
+++ vues/Menu_administrateur.php
@@ -10,7 +10,7 @@
 
 <div id="conteneur">
 	<div class="element"><a href="index.php?cible=capteurs">Capteurs<br/> <img src='style/faq.png'></a></div>
-	<div class="element"><a href="index.php?cible=utilisateurs&fonction=donnees">Donnees<br/> <img src='style/faq.png'></a></div>
+	<div class="element"><a href="index.php?cible=utilisateurs&fonction=donnees_administrateur">Donnees<br/> <img src='style/faq.png'></a></div>
 	<div class="element"><a href="index.php?cible=messagerie">Messagerie<br/> <img src='style/bouton_play.png'></a></div>
 	<div class="element"><a href="index.php?cible=utilisateurs&fonction=utilisateurs">Utilisateurs <br/> <img src='style/bouton_score.png'></a></div>
 	<div class="element"><a href="index.php?cible=utilisateurs&fonction=profilAdmin">Profil<br/> <img src='style/profil.png'></a></div>
