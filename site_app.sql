8a24c8369b853a437b4e68fc6dc7cc7ef0d90406
NathanG5C <55877065+NathanG5C@users.noreply.github.com>
2020-01-23 11:07:48 +0100

    Add files via upload

--- site_app.sql
+++ site_app.sql
@@ -2,8 +2,8 @@
 -- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le :  mer. 22 jan. 2020 à 18:21
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  jeu. 23 jan. 2020 à 10:07
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.3.12
 
@@ -19,16 +19,18 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `site app`
+-- Base de données :  `site_app`
 --
 
 DELIMITER $$
 --
 -- Procédures
 --
+DROP PROCEDURE IF EXISTS `AddGeometryColumn`$$
 CREATE DEFINER=``@`%` PROCEDURE `AddGeometryColumn` (`catalog` VARCHAR(64), `t_schema` VARCHAR(64), `t_name` VARCHAR(64), `geometry_column` VARCHAR(64), `t_srid` INT)  begin
   set @qwe= concat('ALTER TABLE ', t_schema, '.', t_name, ' ADD ', geometry_column,' GEOMETRY REF_SYSTEM_ID=', t_srid); PREPARE ls from @qwe; execute ls; deallocate prepare ls; end$$
 
+DROP PROCEDURE IF EXISTS `DropGeometryColumn`$$
 CREATE DEFINER=``@`%` PROCEDURE `DropGeometryColumn` (`catalog` VARCHAR(64), `t_schema` VARCHAR(64), `t_name` VARCHAR(64), `geometry_column` VARCHAR(64))  begin
   set @qwe= concat('ALTER TABLE ', t_schema, '.', t_name, ' DROP ', geometry_column); PREPARE ls from @qwe; execute ls; deallocate prepare ls; end$$
 
@@ -40,12 +42,15 @@ DELIMITER ;
 -- Structure de la table `capteurs`
 --
 
-CREATE TABLE `capteurs` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `capteurs`;
+CREATE TABLE IF NOT EXISTS `capteurs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_carte` int(11) NOT NULL,
   `type` text NOT NULL,
-  `unité de mesure` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `unité de mesure` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_carte` (`id_carte`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `capteurs`
@@ -67,9 +72,11 @@ INSERT INTO `capteurs` (`id`, `id_carte`, `type`, `unité de mesure`) VALUES
 -- Structure de la table `carte`
 --
 
-CREATE TABLE `carte` (
-  `ID_carte` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `carte`;
+CREATE TABLE IF NOT EXISTS `carte` (
+  `ID_carte` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`ID_carte`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `carte`
@@ -86,12 +93,15 @@ INSERT INTO `carte` (`ID_carte`) VALUES
 -- Structure de la table `connexion`
 --
 
-CREATE TABLE `connexion` (
-  `id_connexion` int(11) NOT NULL,
+DROP TABLE IF EXISTS `connexion`;
+CREATE TABLE IF NOT EXISTS `connexion` (
+  `id_connexion` int(11) NOT NULL AUTO_INCREMENT,
   `id_utilisateur` int(11) NOT NULL,
   `date` date NOT NULL,
-  `heure` time NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `heure` time NOT NULL,
+  PRIMARY KEY (`id_connexion`),
+  KEY `id_utilisateur` (`id_utilisateur`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `connexion`
@@ -137,22 +147,24 @@ INSERT INTO `connexion` (`id_connexion`, `id_utilisateur`, `date`, `heure`) VALU
 -- Structure de la table `faq`
 --
 
-CREATE TABLE `faq` (
-  `ID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `faq`;
+CREATE TABLE IF NOT EXISTS `faq` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Question` text NOT NULL,
-  `Réponse` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `Réponse` text NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `faq`
 --
 
 INSERT INTO `faq` (`ID`, `Question`, `Réponse`) VALUES
-(1, 'Comment faire pour créer un compte ?', 'Pour créer votre compte, veuillez suivre les instructions de la FAQ pour la question \"Comment faire pour créer un compte ?\". Si vous n\'arrivez toujours pas a créer votre compte, tant pis'),
-(2, 'Comment vous contacter ?', 'pour nous contacter vous pouvez utilisez la boite mail intégrer au site, pour cela appuyez simplement sur le bouton correspondant.'),
-(3, 'Comment fonctionne les tests ?', 'Nos appareils sont disponibles uniquement pour les ayant-droit, ceux-ci utilisent des capteurs et dispositifs permettant d\'effectuer les tests. Les résultats sont ensuite directement transmis sur ce site web'),
-(4, 'Comment récupérer mes scores ?', 'Pour récuperer vos scores veuillez allez dans l\'onglet \"score\" de votre profil qui se trouve quelque part sur le site'),
-(5, 'Comment supprimer mon compte ?', 'Vous ne pouvez pas.'),
+(1, 'Comment faire pour créer un compte ?', 'Pour créer votre compte, un administrateur doit être auprès de vous. Il vous demandera les informations nécessaires à la création de votre compte.\r\n'),
+(2, 'Comment nous contacter ?', 'Nous possédons une messagerie interne, cliquez sur le bouton destiné à celle-ci et n\'hésitez pas à nous faire des retours !'),
+(3, 'Comment fonctionne les tests ?', 'Nos appareils sont disponibles uniquement pour les ayant-droit, ceux-ci utilisent des capteurs et dispositifs permettant d\'effectuer les tests. Les résultats sont ensuite directement transmis sur ce site web.'),
+(4, 'Comment récupérer mes scores ?', 'Pour récuperer vos scores veuillez allez dans l\'onglet \"score\" sur la barre de navigation ou depuis l\'accueil.'),
+(5, 'Comment supprimer mon compte ?', 'Il faut demander à un administrateur dans un lieu adéquat.'),
 (6, 'Où doit on effectuer les tests ?', 'Dans un institut spécialisé tel qu\'une auto-école, un hopital etc.');
 
 -- --------------------------------------------------------
@@ -161,13 +173,17 @@ INSERT INTO `faq` (`ID`, `Question`, `Réponse`) VALUES
 -- Structure de la table `messagerie`
 --
 
-CREATE TABLE `messagerie` (
-  `id_message` int(11) NOT NULL,
+DROP TABLE IF EXISTS `messagerie`;
+CREATE TABLE IF NOT EXISTS `messagerie` (
+  `id_message` int(11) NOT NULL AUTO_INCREMENT,
   `message` text NOT NULL,
   `date` datetime NOT NULL,
   `id_envoyeur` int(11) NOT NULL,
-  `id_recepteur` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id_recepteur` int(11) NOT NULL,
+  PRIMARY KEY (`id_message`),
+  KEY `id_envoyeur` (`id_envoyeur`),
+  KEY `id_recepteur` (`id_recepteur`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `messagerie`
@@ -192,7 +208,8 @@ INSERT INTO `messagerie` (`id_message`, `message`, `date`, `id_envoyeur`, `id_re
 -- Structure de la table `score`
 --
 
-CREATE TABLE `score` (
+DROP TABLE IF EXISTS `score`;
+CREATE TABLE IF NOT EXISTS `score` (
   `frequenceCardiaque` int(11) NOT NULL,
   `TemperaturePeau` int(11) NOT NULL,
   `PerceptionAuditiveInf` int(11) NOT NULL,
@@ -201,8 +218,10 @@ CREATE TABLE `score` (
   `ReprodMotifsSonore` int(11) NOT NULL,
   `date` date NOT NULL,
   `id_utilisateur` int(11) NOT NULL,
-  `id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  KEY `id_utilisateur` (`id_utilisateur`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `score`
@@ -217,7 +236,23 @@ INSERT INTO `score` (`frequenceCardiaque`, `TemperaturePeau`, `PerceptionAuditiv
 (72, 39, 433, 17867, 63, 11, '2020-01-22', 6, 6),
 (88, 36, 616, 15689, 59, 17, '2020-01-22', 17, 7),
 (82, 36, 454, 18483, 72, 68, '2020-01-22', 17, 8),
-(94, 39, 292, 17433, 60, 83, '2020-01-22', 17, 9);
+(94, 39, 292, 17433, 60, 83, '2020-01-22', 17, 9),
+(90, 37, 220, 15001, 59, 36, '2020-01-22', 2, 10),
+(99, 33, 221, 15004, 60, 50, '2020-01-22', 2, 11),
+(94, 30, 190, 15004, 65, 50, '2020-01-22', 24, 12),
+(100, 32, 190, 17523, 70, 65, '2020-01-22', 24, 13),
+(85, 36, 199, 15654, 65, 67, '2020-01-22', 7, 14),
+(85, 36, 199, 15654, 65, 67, '2020-01-22', 17, 15),
+(63, 40, 685, 16042, 40, 14, '2020-01-22', 5, 16),
+(87, 36, 215, 16045, 67, 59, '2020-01-22', 23, 17),
+(75, 36, 430, 18947, 79, 12, '2020-01-22', 10, 18),
+(72, 39, 433, 17867, 63, 11, '2020-01-22', 16, 19),
+(99, 33, 221, 15004, 60, 50, '2020-01-22', 9, 20),
+(72, 39, 433, 17867, 63, 11, '2020-01-22', 5, 21),
+(85, 36, 199, 15654, 65, 67, '2020-01-22', 12, 22),
+(87, 36, 215, 16045, 67, 59, '2020-01-22', 13, 23),
+(75, 36, 430, 18947, 79, 12, '2020-01-22', 20, 24),
+(87, 36, 215, 16045, 67, 59, '2020-01-22', 8, 25);
 
 -- --------------------------------------------------------
 
@@ -225,8 +260,9 @@ INSERT INTO `score` (`frequenceCardiaque`, `TemperaturePeau`, `PerceptionAuditiv
 -- Structure de la table `utilisateur`
 --
 
-CREATE TABLE `utilisateur` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(100) NOT NULL,
   `prenom` varchar(100) NOT NULL,
   `mail` text NOT NULL,
@@ -235,125 +271,29 @@ CREATE TABLE `utilisateur` (
   `statut` int(250) NOT NULL,
   `age` int(11) NOT NULL,
   `vision` text NOT NULL,
-  `permis` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `permis` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `utilisateur`
 --
 
 INSERT INTO `utilisateur` (`id`, `nom`, `prenom`, `mail`, `password`, `photo`, `statut`, `age`, `vision`, `permis`) VALUES
-(2, 'ROLLAND', 'Nathan', 'nathan.rolland@gmail.com', 'josuke18!', '', 0, 0, '', ''),
-(5, 'joestar', 'jonathan', 'jonathan.joestar@gmail.com', 'deadlikeashit', '', 0, 0, '', ''),
-(6, 'RORONOA', 'Zoro', 'zoro.roronoa@gmail.com', 'jevaisbattreMihawk', '', 0, 0, '', ''),
-(7, 'nico', 'robin', 'robin.nico@gmail.com', 'jeveuxvivre', '', 0, 0, '', ''),
-(8, 'polnareff', 'jean-pierre', 'jp.polnareff@gmail.com', 'coupedecheveustylé', '', 0, 0, '', ''),
-(9, 'GOLD D', 'Roger', 'goldroger@gmail.com', 'roidespiretes', '', 0, 0, '', ''),
-(10, 'macron', 'emmanuel', 'emmanuel.macron@gmail.com', 'jesuisunebonpresident', '', 0, 0, '', ''),
-(12, 'joestar', 'elisabeth', 'elisabeth.joestar@gmail.com', 'lisalisapower', 'lisa_lisa_9313.jpg', 0, 0, '', ''),
-(13, 'joestar', 'joseph', 'joseph.joestar@gmail.com', 'lisalisaestmamere', 'joseph-joestar-jully-nike.jpg', 0, 0, '', ''),
-(16, 'd\'EPREMESNIL', 'Augustin', 'augustin.depremesnil@gmail.com', 'jotaro19!', 'maxresdefault.jpg', 1, 0, '', ''),
-(17, 'Brando', 'Dio', 'dio.brando@gmail.com', 'jesuisunmechantvampire', 'DioP2.png', 0, 0, '', ''),
-(20, 'Zeppeli', 'Caesar', 'caesar.zeppeli@gmail.com', 'jesuismortenheros', 'e3436924a5bcc49f641cb9da24f10918.png', 0, 0, '', ''),
+(2, 'ROLLAND', 'Nathan', 'nathan.rolland@gmail.com', 'josuke18!', '', 0, 20, '12-9', 'non'),
+(5, 'joestar', 'jonathan', 'jonathan.joestar@gmail.com', 'deadlikeashit', '', 0, 19, '9-9', 'oui'),
+(6, 'RORONOA', 'Zoro', 'zoro.roronoa@gmail.com', 'jevaisbattreMihawk', '', 0, 25, '9-10', 'non'),
+(7, 'nico', 'robin', 'robin.nico@gmail.com', 'jeveuxvivre', '', 0, 23, '2-3', 'non'),
+(8, 'polnareff', 'jean-pierre', 'jp.polnareff@gmail.com', 'coupedecheveustylé', '', 0, 18, '10-9', 'oui'),
+(9, 'GOLD D', 'Roger', 'goldroger@gmail.com', 'roidespiretes', '', 0, 59, '11-11', 'non'),
+(10, 'macron', 'emmanuel', 'emmanuel.macron@gmail.com', 'jesuisunebonpresident', '', 0, 50, '10-9', 'oui'),
+(12, 'joestar', 'elisabeth', 'elisabeth.joestar@gmail.com', 'lisalisapower', 'lisa_lisa_9313.jpg', 0, 21, '14-5', 'oui'),
+(13, 'joestar', 'joseph', 'joseph.joestar@gmail.com', 'lisalisaestmamere', 'joseph-joestar-jully-nike.jpg', 0, 45, '10-10', 'non'),
+(16, 'd\'EPREMESNIL', 'Augustin', 'augustin.depremesnil@gmail.com', 'jotaro19!', 'maxresdefault.jpg', 1, 26, '6-6', 'oui'),
+(17, 'Brando', 'Dio', 'dio.brando@gmail.com', 'jesuisunmechantvampire', 'DioP2.png', 0, 24, '10-10', 'non'),
+(20, 'Zeppeli', 'Caesar', 'caesar.zeppeli@gmail.com', 'jesuismortenheros', 'e3436924a5bcc49f641cb9da24f10918.png', 0, 235, '8-8', 'oui'),
 (23, 'LE FLOCH', 'Stan', 'stanlefloch@gmail.com', 'Joseph20!', 'Joseph_Infobox-0.jpg', 1, 20, '4.5-3.75', 'non'),
-(24, 'Abdul', 'Mohamed', 'mohamed.abdul@gmail.com', 'Jesuismort', 'mohammed-avdol-jojos-bizarre-adventure-stardust-crusaders-49.3.jpg', 0, 0, '', '');
-
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `capteurs`
---
-ALTER TABLE `capteurs`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_carte` (`id_carte`);
-
---
--- Index pour la table `carte`
---
-ALTER TABLE `carte`
-  ADD PRIMARY KEY (`ID_carte`);
-
---
--- Index pour la table `connexion`
---
-ALTER TABLE `connexion`
-  ADD PRIMARY KEY (`id_connexion`),
-  ADD KEY `id_utilisateur` (`id_utilisateur`);
-
---
--- Index pour la table `faq`
---
-ALTER TABLE `faq`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Index pour la table `messagerie`
---
-ALTER TABLE `messagerie`
-  ADD PRIMARY KEY (`id_message`),
-  ADD KEY `id_envoyeur` (`id_envoyeur`),
-  ADD KEY `id_recepteur` (`id_recepteur`);
-
---
--- Index pour la table `score`
---
-ALTER TABLE `score`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_utilisateur` (`id_utilisateur`);
-
---
--- Index pour la table `utilisateur`
---
-ALTER TABLE `utilisateur`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `capteurs`
---
-ALTER TABLE `capteurs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT pour la table `carte`
---
-ALTER TABLE `carte`
-  MODIFY `ID_carte` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT pour la table `connexion`
---
-ALTER TABLE `connexion`
-  MODIFY `id_connexion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
-
---
--- AUTO_INCREMENT pour la table `faq`
---
-ALTER TABLE `faq`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT pour la table `messagerie`
---
-ALTER TABLE `messagerie`
-  MODIFY `id_message` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
--- AUTO_INCREMENT pour la table `score`
---
-ALTER TABLE `score`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT pour la table `utilisateur`
---
-ALTER TABLE `utilisateur`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+(24, 'Abdul', 'Mohamed', 'mohamed.abdul@gmail.com', 'Jesuismort', 'mohammed-avdol-jojos-bizarre-adventure-stardust-crusaders-49.3.jpg', 0, 21, '10-10', 'non');
 
 --
 -- Contraintes pour les tables déchargées
