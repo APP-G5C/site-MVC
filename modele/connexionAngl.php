4982320e1ffec68daedd393651d3cb4bb8ce38a7
redleader14 <46784287+redleader14@users.noreply.github.com>
2020-01-27 21:47:15 +0100

    Delete connexionAngl.php

--- modele/connexionAngl.php
+++
@@ -1,9 +0,0 @@
-<?php
-try
-{
-	$bdd = new PDO('mysql:host=localhost;dbname=site app;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-}
-catch (Exception $e)
-{
-	die('Error : ' . $e->getMessage());
-}
