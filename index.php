e487b232257ccf93e52e277ceae632eb0c16ddc5
redleader14 <46784287+redleader14@users.noreply.github.com>
2020-02-07 15:09:44 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,14 +1,32 @@
 <?php
-
-	ini_set('display_errors',1);
-	include("controleurs/fonctions.php");
 	
-	if(isset($_GET['cible']) && !empty($_GET['cible'])) {
+	ini_set('display_errors', 1);
+	
+	if (isset($_GET['lang'])) {
+		
+		//Si oui, on créer une variable $lang avec pour valeur celle du cookie.
+		$lang = $_GET['lang'];
+		
+	} else {
+		// si le cookie n'existe pas on tente de reconnaitre la langue par défaut du navigateur utilisé
+		$lang = 'fr';
+	}
+	
+	if (isset($_GET['cible']) && !empty($_GET['cible'])) {
 		// Si la variable cible est passé en GET
 		$url = $_GET['cible']; //user, sensor, etc.
 		
 	} else {
 		// Si aucun contrôleur défini en GET, on bascule sur utilisateurs
-		$url = 'utilisateurs';
+		switch ($lang) {
+			case 'fr':
+				$url = 'utilisateurs';
+				break;
+			case 'en':
+				$url = "utilisateursAngl";
+				break;
+		}
+		
 	}
+	
 	include("controleurs/" . $url . ".php");
